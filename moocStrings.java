a3a606f77f18b5c75c09b9f56640076532f0f0fd
aarongc43 <aarongc43@gmail.com>
2021-03-14 11:31:53 -0500

    modified:   moocStrings.java

--- moocStrings.java
+++ moocStrings.java
@@ -44,5 +44,40 @@ public class moocStrings {
         for (int i = 0; i < pieces.length; i++) {
             System.out.println(pieces[i]);
         }
+
+        System.out.println("Enter some text and then enter a string to search for that within your given text.");
+        String stringSearch = Scanner.nextLine();
+        avClub(stringSearch);
+
+        fixedFormatText();
+    }
+
+    public static void avClub (String input) {
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Enter a word to search for that word in your string.");
+        String searchWord = Scanner.nextLine();
+
+        if (input.contains(searchWord)) {
+            System.out.println(searchWord + " was found.");
+        }
+        else {
+            System.out.println(searchWord + " was not found. Try again.");
+        }
+    }
+
+    public static void fixedFormatText () {
+        Scanner Scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = Scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] pieces = input.split(",");
+            System.out.println("Name: " + pieces + pieces[0] + " Age: " + pieces[1])
+        }
     }
 }
