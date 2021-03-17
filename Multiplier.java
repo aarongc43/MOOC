160991874ca1811f2f420bc90eb0c11f6e0cf1c1
aarongc43 <aarongc43@gmail.com>
2021-03-16 20:58:17 -0500

    new file:   Multiplier.class 	new file:   Multiplier.java 	modified:   moocOOP.java

---
+++ Multiplier.java
@@ -0,0 +1,11 @@
+public class Multiplier {
+    private double number;
+    
+    public Multiplier(double number) {
+        this.number = number;
+    }
+
+    public double multiply(double number) {
+        return this.number * number;
+    }
+}
--- moocOOP.java
+++ moocOOP.java
@@ -89,6 +89,17 @@ public class moocOOP {
         System.out.println(bond);
 
         //Method Parameters
+        
+        Multiplier multiplyByThree = new Multiplier(3);
 
+        System.out.println("multiplyByThree.multiply(2): " + multiplyByThree.multiply(2));
+        Multiplier multiplyByFour = new Multiplier(4);
+
+        System.out.println("multiplyByFour.multiply(2): " + multiplyByFour.multiply(2));
+        System.out.println("multiplyByThree.multiply(1): " + multiplyByThree.multiply(1));
+        System.out.println("multiplyByFour.multiply(1): " + multiplyByFour.multiply(1));
+        
+        // Calling an internal Method
+        
     }
 }
