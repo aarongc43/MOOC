1ebd323f390601d64205bc7ac8b4dc53886b4976
aarongc43 <aarongc43@gmail.com>
2021-03-24 20:38:47 -0500

    new file:   Part04_19.Books/Book.java 	new file:   Part04_19.Books/Main.java

---
+++ Part04_19.Books/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    
+    private String name;
+    private int pages;
+    private int year;
+
+    public Book (String name, int pages, int year) {
+        this.name = name;
+        this.pages = pages;
+        this.year = year;
+    }
+
+
+}
---
+++ Part04_19.Books/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        // Read book info from user: Title, Pages, Publication Year
+        // User then asked to print what is to be printed after done with books
+        // "everything", "name": Book titles, "
+    }
+}
